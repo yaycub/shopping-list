258ea47d3d383e076c954051b2afb00afa22ff8d
Jacob Harrington <jharrington4@gmail.com>
2019-07-07 13:00:10 -0700

    updating index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+$('#js-shopping-list-form').submit(function(event)) {
+  event.preventDefault();
+  $('.shopping-list').append(
+    '<li>' +
+        '<span class="shopping-item">' + $('shopping-list-entry').val() + '</span>' +
+        '<div class="shopping-item-controls"> +
+          '<button class="shopping-item-toggle">' +
+            '<span class="button-label">check</span>' +
+          '</button>' +
+          '<button class="shopping-item-delete">' +
+            '<span class="button-label">delete</span>' +
+          '</button>' +
+        '</div>' + 
+      '</li>'
+  );
+});
+}
